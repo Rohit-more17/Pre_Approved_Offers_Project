382b3cc44a49b07328c3fdceec067aa33f13a312
Rohit-more17 <rohitmore7000@gmail.com>
2022-10-13 15:59:50 +0530

    Drop down fixed

--- App.js
+++ App.js
@@ -1,13 +1,16 @@
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
 import HomeScreen from "./src/screens/HomeScreen";
+import PracticeScreen from "./src/screens/PracticeScreen";
 import TempScreen from "./src/screens/TempScreen";
+
 
 
 const navigator = createStackNavigator(
   {
     Home : HomeScreen,
     Temp : TempScreen,
+    Prac : PracticeScreen ,
   },
   {
     initialRouteName :"Home",
